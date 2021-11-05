058c240d1ac116494c567e91d7bf896fb275b0a6
varshini <dreamvarshini@gmail.com>
2021-11-05 18:20:09 +0530

    MODIFIED: storing contact data in memory

--- src/main/java/com/bridgelabz/addressbook/service/AddressBookService.java
+++ src/main/java/com/bridgelabz/addressbook/service/AddressBookService.java
@@ -11,39 +11,44 @@ import org.springframework.stereotype.Service;
 public class AddressBookService implements IAddressBookService
 {
 
+    List<Contact> contactList = new ArrayList<>();
+
     @Override
     public List<Contact> getContact() 
     {
-        List<Contact> contactList = new ArrayList<>();
-        contactList.add(new Contact(1,new ContactDTO("Ted","Mosby","Abc street,12th cross")));
         return contactList;
     }
 
     @Override
     public Contact getContactById(int contactId) 
     {
-        Contact contact = new Contact(1,new ContactDTO("Ted","Mosby","Abc street,12th cross"));
-        return contact;
+        return contactList.get(contactId-1);
     }
 
     @Override
     public Contact createContact(ContactDTO contactDTO) 
     {
-        Contact contact = new Contact(1,contactDTO);
+        Contact contact = new Contact(contactList.size()+1,contactDTO);
+        contactList.add(contact);
         return contact;
+
     }
 
     @Override
     public Contact updateContact(int contactId, ContactDTO contactDTO) 
     {
-        Contact contact = new Contact(1,contactDTO);
+        Contact contact = this.getContactById(contactId);
+        contact.setFirstName(contactDTO.firstName);
+        contact.setLastName(contactDTO.lastName);
+        contact.setAddress(contactDTO.address);
+        contactList.set(contactId-1,contact);
         return contact;
     }
 
     @Override
     public void deleteContact(int contactId) 
     {
-        
+        contactList.remove(contactId-1);   
         
     }
 
