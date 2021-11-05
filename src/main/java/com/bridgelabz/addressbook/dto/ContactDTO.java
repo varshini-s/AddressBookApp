fcc1e51ac5ec67e64d52a023a16fab291d98f4d4
varshini <dreamvarshini@gmail.com>
2021-11-05 20:30:13 +0530

    ADDED: validations for contact dto class

--- src/main/java/com/bridgelabz/addressbook/dto/ContactDTO.java
+++ src/main/java/com/bridgelabz/addressbook/dto/ContactDTO.java
@@ -1,12 +1,23 @@
 package com.bridgelabz.addressbook.dto;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 public class ContactDTO 
 {
+	@NotEmpty(message = "Contact name cannot be null")
+	@Pattern(regexp = "^[A-Z]{1}[a-z]{2,}$",message = "first name Invalid")
     public String firstName;
+
+	@NotEmpty(message = "Contact name cannot be null")
+	@Pattern(regexp = "^[A-Z]{1}[a-z]{2,}$",message = "last name Invalid")
     public String lastName;
+	
+	@Pattern(regexp = "(?!^\\d+$)^[A-Z,a-z,0-9, ()#-]{3,}$",message = "Address Invalid")
     public String address;
     
-	public ContactDTO(String firstName, String lastName, String address) {
+	public ContactDTO(String firstName, String lastName, String address) 
+	{
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -14,7 +25,8 @@ public class ContactDTO
 	}
 
 	@Override
-	public String toString() {
+	public String toString() 
+	{
 		return "ContactDTO [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + "]";
 	}
 
