325af519174fd07b43254276341c9bcb083c6692
varshini <dreamvarshini@gmail.com>
2021-11-08 15:38:12 +0530

    REFACTORED: using @Data annotation instead of getters setters

--- src/main/java/com/bridgelabz/addressbook/model/Contact.java
+++ src/main/java/com/bridgelabz/addressbook/model/Contact.java
@@ -2,7 +2,9 @@ package com.bridgelabz.addressbook.model;
 
 import com.bridgelabz.addressbook.dto.ContactDTO;
 
-public class Contact 
+import lombok.Data;
+
+public @Data class Contact 
 {
 	private int contactId;
 	private String name;
@@ -11,6 +13,11 @@ public class Contact
 	private String city;
 	private String state;
 	private String zip;
+
+	public Contact()
+	{
+
+	}
     
 	public Contact(int contactId,ContactDTO contactDTO) 
 	{
@@ -23,78 +30,5 @@ public class Contact
 		this.zip = contactDTO.zip;
 	}
 
-	public int getContactId() 
-	{
-		return contactId;
-	}
-
-	public void setContactId(int contactId) 
-	{
-		this.contactId = contactId;
-	}
-
-	public String getName() 
-	{
-		return name;
-	}
-
-	public void setName(String name) 
-	{
-		this.name = name;
-	}
-
-	public String getAddress() 
-	{
-		return address;
-	}
-
-	public void setAddress(String address) 
-	{
-		this.address = address;
-	}
-
-	public String getPhoneNumber() 
-	{
-		return phoneNumber;
-	}
-
-	public void setPhoneNumber(String phoneNumber) 
-	{
-		this.phoneNumber = phoneNumber;
-	}
-
-	public String getCity() 
-	{
-		return city;
-	}
-
-	public void setCity(String city)
-	{
-		this.city = city;
-	}
-
-	public String getState() 
-	{
-		return state;
-	}
-
-	public void setState(String state) 
-	{
-		this.state = state;
-	}
-
-	public String getZip() 
-	{
-		return zip;
-	}
-
-	public void setZip(String zip) 
-	{
-		this.zip = zip;
-	}
-
-
-    
-    
 
 }
